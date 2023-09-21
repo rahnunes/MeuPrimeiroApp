102ada01525e6bf215c5c92010fdbaa62afc1db4
rahnunes <nunesrayssa499@gmail.com>
2023-09-21 09:08:34 -0300

    oii

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Rayssa")
                 }
             }
         }
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: Rayssa, modifier: Modifier = Modifier) {
+fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
